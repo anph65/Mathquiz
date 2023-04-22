5957c7c4f03b5a8625c915316da3235f67fb1ba0
anph65 <131370020+anph65@users.noreply.github.com>
2023-04-22 23:36:04 +0900

    Update MathQuiz.py

--- MathQuiz.py
+++ MathQuiz.py
@@ -1,63 +1,42 @@
 import random
+import streamlit as st
 
 def math_quiz():
-    while True:
-        try:
-            level = int(input("Select difficulty level: 1-Easy, 2-Medium, 3-Hard\n"))
-            if level not in [1, 2, 3]:
-                raise ValueError
-            break
-        except ValueError:
-            print("Please enter only 1, 2 or 3 !")
-    
-    if level == 1:
+    level = st.sidebar.selectbox("Select difficulty level:", ["Easy", "Medium", "Hard"])
+    if level == "Easy":
         num1 = random.randint(1, 10)
         num2 = random.randint(1, 10)
-    elif level == 2:
+    elif level == "Medium":
         num1 = random.randint(1, 30)
         num2 = random.randint(1, 30)
-    elif level == 3:
+    elif level == "Hard":
         num1 = random.randint(1, 100)
         num2 = random.randint(1, 100)
-    
+
     question1 = f"What is {num1}+{num2}?"
     answer1 = num1 + num2
     question2 = f"What is {num1}*{num2}?"
     answer2 = num1 * num2
     question3 = f"What is {num1}/5-{num2}/10?"
     answer3 = round(num1/5 - num2/10, 2)
-    
-    print(question1)
-    while True:
-        try:
-            user_answer1 = float(input())
-            break
-        except ValueError:
-            print("Please enter the number only")
-    if user_answer1 != answer1:
-        print("Gameover!")
-        return
-    print(question2)
-    while True:
-        try:
-            user_answer2 = float(input())
-            break
-        except ValueError:
-            print("Please enter the number only")
-    if user_answer2 != answer2:
-        print("Gameover!")
-        return
-    print(question3)
-    while True:
-        try:
-            user_answer3 = float(input())
-            break
-        except ValueError:
-            print("Please enter the number only")
-    if user_answer3 != answer3:
-        print("Gameover!")
-        return
-    
-    print("Congratulations! You are winner!")
 
-math_quiz()
+    st.write(question1)
+    user_answer1 = st.number_input("Enter your answer:", value=0.0, step=1.0)
+    if user_answer1 != answer1 and user_answer1 != 0:
+        st.write("Gameover!")
+        return
+    st.write(question2)
+    user_answer2 = st.number_input("Enter your answer:", value=0.0, step=1.0)
+    if user_answer2 != answer2 and user_answer2 != 0:
+        st.write("Gameover!")
+        return
+    st.write(question3)
+    user_answer3 = st.number_input("Enter your answer:", value=0.0, step=0.01)
+    if user_answer3 != answer3 and user_answer3 != 0:
+        st.write("Gameover!")
+        return
+    
+    st.write("Congratulations! You are winner!")
+
+if __name__ == "__main__":
+    math_quiz()
